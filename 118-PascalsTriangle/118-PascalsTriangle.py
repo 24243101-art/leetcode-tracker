9b24e1a31ee3920a1235ac16406e9cb504286e82
24243101-art <24243101@drngpit.ac.in>
2026-07-09 10:08:32 +0530

    Create 118-PascalsTriangle

---
+++ 118-PascalsTriangle/118-PascalsTriangle.py
@@ -0,0 +1,12 @@
+# Last updated: 7/9/2026, 10:08:32 AM
+class Solution:
+    def generate(self, numRows):
+        triangle = []
+
+        for i in range(numRows):
+            row = [1] * (i + 1)
+            for j in range(1, i):
+                row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
+            triangle.append(row)
+
+        return triangle
