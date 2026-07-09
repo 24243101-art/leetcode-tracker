d820d3ae79f248e74468bb680455a53aad4927d8
24243101-art <24243101@drngpit.ac.in>
2026-07-09 10:08:57 +0530

    Create 103-BinaryTreeZigzagLevelOrderTraversal

---
+++ 103-BinaryTreeZigzagLevelOrderTraversal/103-BinaryTreeZigzagLevelOrderTraversal.py
@@ -0,0 +1,26 @@
+# Last updated: 7/9/2026, 10:08:57 AM
+class Solution:
+    def zigzagLevelOrder(self, root):
+        if not root:
+            return []
+        
+        result = []
+        queue = [root]
+        left_to_right = True
+        
+        while queue:
+            level = []
+            for _ in range(len(queue)):
+                node = queue.pop(0)
+                level.append(node.val)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            if not left_to_right:
+                level.reverse()
+            
+            result.append(level)
+            left_to_right = not left_to_right 
+        
+        return result
