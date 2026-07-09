9bbd5433648fc312ba13c1c747f00f8eeb289662
24243101-art <24243101@drngpit.ac.in>
2026-07-09 10:08:08 +0530

    Create 133-CloneGraph

---
+++ 133-CloneGraph/133-CloneGraph.py
@@ -0,0 +1,26 @@
+# Last updated: 7/9/2026, 10:08:08 AM
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+
+class Solution:
+    def cloneGraph(self, node):
+        if not node:
+            return None
+
+        visited = {}
+
+        def dfs(curr):
+            if curr in visited:
+                return visited[curr]
+
+            clone = Node(curr.val)
+            visited[curr] = clone
+
+            for neighbor in curr.neighbors:
+                clone.neighbors.append(dfs(neighbor))
+
+            return clone
+
+        return dfs(node)
