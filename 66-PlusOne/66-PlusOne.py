ef9e6205cc0e94c14af4289503697375957468ed
24243101-art <24243101@drngpit.ac.in>
2026-07-09 10:10:25 +0530

    Create 66-PlusOne

---
+++ 66-PlusOne/66-PlusOne.py
@@ -0,0 +1,9 @@
+# Last updated: 7/9/2026, 10:10:25 AM
+class Solution(object):
+    def plusOne(self, digits):
+        for i in range(len(digits) - 1, -1, -1):
+            if digits[i] < 9:
+                digits[i] += 1
+                return digits
+            digits[i] = 0
+        return [1] + digits
