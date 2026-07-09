b1552db4bdffbf813b5094f545ef4f722084599b
24243101-art <24243101@drngpit.ac.in>
2026-07-09 10:12:14 +0530

    Create 11-ContainerWithMostWater

---
+++ 11-ContainerWithMostWater/11-ContainerWithMostWater.py
@@ -0,0 +1,18 @@
+# Last updated: 7/9/2026, 10:12:14 AM
+class Solution(object):
+    def maxArea(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        left, right = 0, len(height) - 1
+        max_area = 0
+        while left < right:
+            h = min(height[left], height[right])
+            w = right - left
+            max_area = max(max_area, h * w)
+            if height[left] < height[right]:
+                left += 1
+            else:
+                right -= 1
+        return max_area
