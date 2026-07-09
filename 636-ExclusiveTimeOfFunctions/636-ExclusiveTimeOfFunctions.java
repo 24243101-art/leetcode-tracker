f3eb0b8af1104e991278fb8a70972dfae9559854
24243101-art <24243101@drngpit.ac.in>
2026-07-09 10:07:27 +0530

    Create 636-ExclusiveTimeOfFunctions

---
+++ 636-ExclusiveTimeOfFunctions/636-ExclusiveTimeOfFunctions.java
@@ -0,0 +1,32 @@
+// Last updated: 7/9/2026, 10:07:26 AM
+import java.util.*;
+
+class Solution {
+    public int[] exclusiveTime(int n, List<String> logs) {
+        int[] res = new int[n];
+        Stack<Integer> stack = new Stack<>();
+        
+        int prevTime = 0;
+
+        for (String log : logs) {
+            String[] parts = log.split(":");
+            int id = Integer.parseInt(parts[0]);
+            String type = parts[1];
+            int time = Integer.parseInt(parts[2]);
+
+            if (type.equals("start")) {
+                if (!stack.isEmpty()) {
+                    res[stack.peek()] += time - prevTime;
+                }
+                stack.push(id);
+                prevTime = time;
+            } 
+            else { 
+                res[stack.pop()] += time - prevTime + 1;
+                prevTime = time + 1;
+            }
+        }
+
+        return res;
+    }
+}
