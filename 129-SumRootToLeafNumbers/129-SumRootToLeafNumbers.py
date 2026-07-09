efff0778a5a95f6b03ef0db1a82b300532755356
24243101-art <24243101@drngpit.ac.in>
2026-07-09 10:08:15 +0530

    Create 129-SumRootToLeafNumbers

---
+++ 129-SumRootToLeafNumbers/129-SumRootToLeafNumbers.py
@@ -0,0 +1,21 @@
+# Last updated: 7/9/2026, 10:08:15 AM
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def sumNumbers(self, root):
+        def dfs(node, current_sum):
+            if not node:
+                return 0
+            
+            current_sum = current_sum * 10 + node.val
+            
+            if not node.left and not node.right:
+                return current_sum
+            
+            return dfs(node.left, current_sum) + dfs(node.right, current_sum)
+        
+        return dfs(root, 0)
