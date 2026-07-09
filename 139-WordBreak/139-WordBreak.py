a7926e45a6bb86b6a90b6f7e7f54233ea8949d4d
24243101-art <24243101@drngpit.ac.in>
2026-07-09 10:07:58 +0530

    Create 139-WordBreak

---
+++ 139-WordBreak/139-WordBreak.py
@@ -0,0 +1,15 @@
+# Last updated: 7/9/2026, 10:07:57 AM
+class Solution:
+    def wordBreak(self, s, wordDict):
+        word_set = set(wordDict)   
+        n = len(s)
+        dp = [False] * (n + 1)
+        dp[0] = True  
+
+        for i in range(1, n + 1):
+            for j in range(i):
+                if dp[j] and s[j:i] in word_set:
+                    dp[i] = True
+                    break
+
+        return dp[n]
