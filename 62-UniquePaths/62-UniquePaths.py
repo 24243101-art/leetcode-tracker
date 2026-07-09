c61cbcb566177a92d9e0d4aacb637646581938a6
24243101-art <24243101@drngpit.ac.in>
2026-07-09 10:10:35 +0530

    Create 62-UniquePaths

---
+++ 62-UniquePaths/62-UniquePaths.py
@@ -0,0 +1,8 @@
+# Last updated: 7/9/2026, 10:10:34 AM
+class Solution(object):
+    def uniquePaths(self, m, n):
+        dp = [[1] * n for _ in range(m)]
+        for i in range(1, m):
+            for j in range(1, n):
+                dp[i][j] = dp[i-1][j] + dp[i][j-1]
+        return dp[m-1][n-1]
