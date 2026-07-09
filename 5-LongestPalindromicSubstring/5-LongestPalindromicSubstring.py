987691a2c9ec91c44a55e29a8b1d39460d5ef073
24243101-art <24243101@drngpit.ac.in>
2026-07-09 10:12:26 +0530

    Create 5-LongestPalindromicSubstring

---
+++ 5-LongestPalindromicSubstring/5-LongestPalindromicSubstring.py
@@ -0,0 +1,27 @@
+# Last updated: 7/9/2026, 10:12:26 AM
+class Solution(object):
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        def expandAroundCenter(left, right):
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                left -= 1
+                right += 1
+            return s[left + 1:right]
+
+        longest = ""
+        for i in range(len(s)):
+            # Odd-length palindrome
+            p1 = expandAroundCenter(i, i)
+            # Even-length palindrome
+            p2 = expandAroundCenter(i, i + 1)
+
+            if len(p1) > len(longest):
+                longest = p1
+            if len(p2) > len(longest):
+                longest = p2
+
+        return longest
+        
