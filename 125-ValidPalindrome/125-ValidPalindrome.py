65109648e56725248280bba498aa7474b9317c13
24243101-art <24243101@drngpit.ac.in>
2026-07-09 10:08:20 +0530

    Create 125-ValidPalindrome

---
+++ 125-ValidPalindrome/125-ValidPalindrome.py
@@ -0,0 +1,16 @@
+# Last updated: 7/9/2026, 10:08:20 AM
+class Solution:
+    def isPalindrome(self, s):
+        left, right = 0, len(s) - 1
+        while left < right:
+            while left < right and not s[left].isalnum():
+                left += 1
+            while left < right and not s[right].isalnum():
+                right -= 1            
+            if s[left].lower() != s[right].lower():
+                return False
+            
+            left += 1
+            right -= 1
+        
+        return True
