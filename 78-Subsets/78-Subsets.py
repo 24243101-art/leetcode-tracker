599bf37f8e1a9adc370733bb00215a1a1a95648b
24243101-art <24243101@drngpit.ac.in>
2026-07-09 10:09:45 +0530

    Create 78-Subsets

---
+++ 78-Subsets/78-Subsets.py
@@ -0,0 +1,14 @@
+# Last updated: 7/9/2026, 10:09:45 AM
+class Solution(object):
+    def subsets(self, nums):
+        res = []
+        
+        def backtrack(start, path):
+            res.append(list(path))
+            for i in range(start, len(nums)):
+                path.append(nums[i])
+                backtrack(i + 1, path)
+                path.pop()
+        
+        backtrack(0, [])
+        return res
