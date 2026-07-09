f1edb11004d00fbd01d93809c0b43b31e5bb5bc9
24243101-art <24243101@drngpit.ac.in>
2026-07-09 10:08:13 +0530

    Create 130-SurroundedRegions

---
+++ 130-SurroundedRegions/130-SurroundedRegions.py
@@ -0,0 +1,30 @@
+# Last updated: 7/9/2026, 10:08:13 AM
+class Solution:
+    def solve(self, board):
+        if not board or not board[0]:
+            return
+        
+        m, n = len(board), len(board[0])
+        
+        def dfs(r, c):
+            if r < 0 or r >= m or c < 0 or c >= n or board[r][c] != "O":
+                return
+            board[r][c] = "E"  
+            dfs(r+1, c)
+            dfs(r-1, c)
+            dfs(r, c+1)
+            dfs(r, c-1)
+        
+        for i in range(m):
+            dfs(i, 0)
+            dfs(i, n-1)
+        for j in range(n):
+            dfs(0, j)
+            dfs(m-1, j)
+        
+        for i in range(m):
+            for j in range(n):
+                if board[i][j] == "O":
+                    board[i][j] = "X"
+                elif board[i][j] == "E":
+                    board[i][j] = "O"
