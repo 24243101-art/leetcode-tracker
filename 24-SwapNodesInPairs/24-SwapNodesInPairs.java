0849ef85dce1ccca3c352404a7a83048a4d7858a
24243101-art <24243101@drngpit.ac.in>
2026-07-09 10:11:42 +0530

    Create 24-SwapNodesInPairs

---
+++ 24-SwapNodesInPairs/24-SwapNodesInPairs.java
@@ -0,0 +1,19 @@
+// Last updated: 7/9/2026, 10:11:41 AM
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode prev = dummy;
+
+        while (prev.next != null && prev.next.next != null) {
+            ListNode first = prev.next;
+            ListNode second = prev.next.next;
+            first.next = second.next;
+            second.next = first;
+            prev.next = second;
+            prev = first;
+        }
+
+        return dummy.next;
+    }
+}
