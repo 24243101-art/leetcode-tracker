225c3d316f7500cc84ab47e1a47912d22e14675d
24243101-art <24243101@drngpit.ac.in>
2026-07-09 10:10:45 +0530

    Create 56-MergeIntervals

---
+++ 56-MergeIntervals/56-MergeIntervals.py
@@ -0,0 +1,13 @@
+# Last updated: 7/9/2026, 10:10:44 AM
+class Solution(object):
+    def merge(self, intervals):
+        intervals.sort(key=lambda x: x[0])
+        merged = []
+
+        for interval in intervals:
+            if not merged or merged[-1][1] < interval[0]:
+                merged.append(interval)
+            else:
+                merged[-1][1] = max(merged[-1][1], interval[1])
+
+        return merged
