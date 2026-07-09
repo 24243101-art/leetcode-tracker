8d96410c22f324ab965c490766ebf21aef3dccab
24243101-art <24243101@drngpit.ac.in>
2026-07-09 10:11:01 +0530

    Create 48-RotateImage

---
+++ 48-RotateImage/48-RotateImage.py
@@ -0,0 +1,10 @@
+# Last updated: 7/9/2026, 10:11:00 AM
+class Solution(object):
+    def rotate(self, matrix):
+        n = len(matrix)
+
+        for i in range(n):
+            for j in range(i + 1, n):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+        for row in matrix:
+            row.reverse()
