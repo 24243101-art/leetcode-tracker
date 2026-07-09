2f76b1609360c564a22ec6bcdffe4c3213d2e6ac
24243101-art <24243101@drngpit.ac.in>
2026-07-09 10:08:55 +0530

    Create 104-MaximumDepthOfBinaryTree

---
+++ 104-MaximumDepthOfBinaryTree/104-MaximumDepthOfBinaryTree.py
@@ -0,0 +1,14 @@
+# Last updated: 7/9/2026, 10:08:53 AM
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def maxDepth(self, root):
+        if not root:
+            return 0
+        left_depth = self.maxDepth(root.left)
+        right_depth = self.maxDepth(root.right)
+        return 1 + max(left_depth, right_depth)
