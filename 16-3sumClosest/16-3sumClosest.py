f4e969c7230671b854c6f4fe7c180d75ad625805
24243101-art <24243101@drngpit.ac.in>
2026-07-09 10:11:55 +0530

    Create 16-3sumClosest

---
+++ 16-3sumClosest/16-3sumClosest.py
@@ -0,0 +1,30 @@
+# Last updated: 7/9/2026, 10:11:54 AM
+class Solution(object):
+    def threeSumClosest(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        nums.sort()
+        closest_sum = nums[0] + nums[1] + nums[2]
+        
+        for i in range(len(nums) - 2):
+            left = i + 1
+            right = len(nums) - 1
+            
+            while left < right:
+                curr_sum = nums[i] + nums[left] + nums[right]
+                
+                if abs(curr_sum - target) < abs(closest_sum - target):
+                    closest_sum = curr_sum
+                
+                if curr_sum < target:
+                    left += 1
+                elif curr_sum > target:
+                    right -= 1
+                else:
+                    # Exact match found
+                    return curr_sum
+        
+        return closest_sum
