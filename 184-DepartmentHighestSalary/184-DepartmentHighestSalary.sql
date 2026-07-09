44b7ab64c95d7b5b84ed0ff465ba806df4209574
24243101-art <24243101@drngpit.ac.in>
2026-07-09 10:07:34 +0530

    Create 184-DepartmentHighestSalary

---
+++ 184-DepartmentHighestSalary/184-DepartmentHighestSalary.sql
@@ -0,0 +1,13 @@
+-- Last updated: 7/9/2026, 10:07:34 AM
+SELECT Department, Employee, Salary
+FROM (
+    SELECT 
+        d.name AS Department,
+        e.name AS Employee,
+        e.salary AS Salary,
+        RANK() OVER (PARTITION BY e.departmentId ORDER BY e.salary DESC) AS rnk
+    FROM Employee e
+    JOIN Department d
+    ON e.departmentId = d.id
+) t
+WHERE rnk = 1;
