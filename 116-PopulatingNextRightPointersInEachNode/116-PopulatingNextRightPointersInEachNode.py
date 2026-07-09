b5b71771318428cd60d0a211c1e32914d10ef189
24243101-art <24243101@drngpit.ac.in>
2026-07-09 10:08:35 +0530

    Create 116-PopulatingNextRightPointersInEachNode

---
+++ 116-PopulatingNextRightPointersInEachNode/116-PopulatingNextRightPointersInEachNode.py
@@ -0,0 +1,28 @@
+# Last updated: 7/9/2026, 10:08:35 AM
+class Node:
+    def __init__(self, val=0, left=None, right=None, next=None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+
+class Solution:
+    def connect(self, root):
+        if not root:
+            return None
+        
+        leftmost = root
+        
+        while leftmost.left:
+            head = leftmost
+            while head:
+                head.left.next = head.right
+                
+                if head.next:
+                    head.right.next = head.next.left
+                
+                head = head.next
+            
+            leftmost = leftmost.left
+        
+        return root
