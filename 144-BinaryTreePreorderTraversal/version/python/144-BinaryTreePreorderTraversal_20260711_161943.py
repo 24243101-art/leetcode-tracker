f353f79d6f80d7a3fc9dae3b6620a0538b667e12
24243101-art <24243101@drngpit.ac.in>
2026-07-11 16:19:43 +0530

    Create 144-BinaryTreePreorderTraversal

---
+++ 144-BinaryTreePreorderTraversal/version/python/144-BinaryTreePreorderTraversal_20260711_161943.py
@@ -0,0 +1,14 @@
+# Last updated: 7/11/2026, 4:19:43 PM
+1class Solution:
+2    def preorderTraversal(self, root):
+3        result = []
+4
+5        def preorder(node):
+6            if not node:
+7                return
+8            result.append(node.val)
+9            preorder(node.left)
+10            preorder(node.right)
+11
+12        preorder(root)
+13        return result
