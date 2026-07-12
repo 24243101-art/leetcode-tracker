c31b53de16b996ea3118298458dcb97ed22f55ec
24243101-art <24243101@drngpit.ac.in>
2026-07-12 09:30:36 +0530

    Create

---
+++ problem-1783828835983/version/python/problem-1783828835983_20260712_093035.py
@@ -0,0 +1,12 @@
+# Last updated: 7/12/2026, 9:30:35 AM
+1class Solution(object):
+2    def maxConsistentColumns(self, grid, limit):
+3        R = len(grid)
+4        C = len(grid[0])
+5        dp = [1] * C
+6        for j in range(1, C):
+7            for i in range(j):
+8                if all(abs(grid[r][i] - grid[r][j]) <= limit for r in range(R)):
+9                    dp[j] = max(dp[j], dp[i] + 1)
+10        return max(dp)
+11        
