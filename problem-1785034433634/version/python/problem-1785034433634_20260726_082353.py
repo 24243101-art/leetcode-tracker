9c6e2d05b9e3203b8e14d93174ea8c0b9f17a248
24243101-art <24243101@drngpit.ac.in>
2026-07-26 08:23:54 +0530

    Create

---
+++ problem-1785034433634/version/python/problem-1785034433634_20260726_082353.py
@@ -0,0 +1,16 @@
+# Last updated: 7/26/2026, 8:23:53 AM
+1class Solution:
+2    def aggregateTimeSeries(self, series1: list[list[int]], series2: list[list[int]]) -> list[list[int]]:
+3        timestamps = sorted(list(set([item[0] for item in series1] + [item[0] for item in series2])))
+4        result = []
+5        i, j = 0, 0
+6        n1, n2 = len(series1), len(series2)
+7        for t in timestamps:
+8            while i < n1 and series1[i][0] < t:
+9                i += 1
+10            while j < n2 and series2[j][0] < t:
+11                j += 1
+12            val1 = series1[i][1] if i < n1 else 0
+13            val2 = series2[j][1] if j < n2 else 0
+14            result.append([t, val1 + val2])
+15        return result
