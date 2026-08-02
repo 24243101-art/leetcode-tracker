8b5d4d4d15612ee9316192fc030c5d4c17117cd7
24243101-art <24243101@drngpit.ac.in>
2026-08-02 08:06:37 +0530

    Create

---
+++ problem-1785638196633/version/python/problem-1785638196633_20260802_080636.py
@@ -0,0 +1,13 @@
+# Last updated: 8/2/2026, 8:06:36 AM
+1class Solution:
+2    def maxPairStrength(self, nums: list[int]) -> int:
+3        max_strength = 0
+4        n = len(nums)
+5        for i in range(n):
+6            for j in range(i + 1, n):
+7                a, b = nums[i], nums[j]
+8                g = math.gcd(a, b)
+9                current_strength = (a * b) // (g * g)
+10                if current_strength > max_strength:
+11                    max_strength = current_strength
+12        return max_strength
